2e6064ce3c7bc27788151227d747e4812120d65c
tetsuromori <tetsuromori@gmail.com>
2018-09-19 13:27:43 +0900

    Update validate solution

--- validate/index.js
+++ validate/index.js
@@ -5,41 +5,41 @@
 // every node's right hand child is greater than
 // the parent
 
-// /* Stephen's Solution */
-// function validate(node, min = null, max = null){
-//   if(max !== null && node.data > max) return false;
-//   if(min !== null && node.data < min) return false;
-//
-//   if(node.left && !validate(node.left, min, node.data)) return false;
-//   if(node.right && !validate(node.right, node.data, max)) return false;
-//
-//   return true;
-// }
-
-/* My Solution */
+/* Stephen's Solution */
 function validate(node, min = null, max = null){
-  if(min !== null && node.data < min) return false;
-  if(max !== null && node.data > max) return false;
+  if(max && node.data > max) return false;
+  if(min && node.data < min) return false;
 
-  let left = null;
-  let right = null;
+  if(node.left && !validate(node.left, min, node.data)) return false;
+  if(node.right && !validate(node.right, node.data, max)) return false;
 
-  if(!node.left) {
-    left = true;
-  } else if(node.left.data > node.data) {
-    left = false;
-  } else {
-    left = validate(node.left, min, node.data);
-  }
-
-  if(!node.right) {
-    right = true;
-  } else if(node.data > node.right.data) {
-    right = false;
-  } else {
-    right = validate(node.right, node.data, max);
-  }
-  return left && right;
+  return true;
 }
+
+// /* My Solution */
+// function validate(node, min = null, max = null){
+//   if(min && node.data < min) return false;
+//   if(max && node.data > max) return false;
+//
+//   let left = null;
+//   let right = null;
+//
+//   if(!node.left) {
+//     left = true;
+//   } else if(node.left.data > node.data) {
+//     left = false;
+//   } else {
+//     left = validate(node.left, min, node.data);
+//   }
+//
+//   if(!node.right) {
+//     right = true;
+//   } else if(node.data > node.right.data) {
+//     right = false;
+//   } else {
+//     right = validate(node.right, node.data, max);
+//   }
+//   return left && right;
+// }
 
 module.exports = validate;
